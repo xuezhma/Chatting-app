1fec337c5d858c1098bffe853bd4eb1c154c2f35
xuezhma <xuezheng-ma@uiowa.edu>
2015-10-30 22:35:56 -0500

    Live Messages

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(){
-	console.log("User connected via socket!")
+io.on('connection', function(socket){
+	console.log("User connected via socket!");
+
+	socket.on('message', function(message){
+		console.log('Message receieved: ' + message.text);
+		//io.emit sends to everyone, socket.broadcast send to other users only;
+		socket.broadcast.emit('message',message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to my chat app!'
+	});
+
+
+	
 });
 
 http.listen(PORT, function(){
